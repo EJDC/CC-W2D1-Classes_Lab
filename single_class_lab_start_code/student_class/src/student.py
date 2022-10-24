67a666a6fe748548b70be434a292868cdbe2b939
user <user@users-MacBook-Pro.local>
2022-10-24 15:23:47 +0100

    First Commit

---
+++ single_class_lab_start_code/student_class/src/student.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+
+
+class Student:
+    def __init__(self, name, cohort): 
+        self.name = name
+        self.cohort = cohort
+    
+    def talk(self):
+        return("I can talk!")
+    
+    def say_favourite_language(self, fav_lang):
+        return("I love " + fav_lang)
+
+    
